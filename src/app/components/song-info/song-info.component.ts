3ce9d6e1b5a9e75b36562b6cf2cd6a1c57ff5a8d
Nursultan Turdaliev <nursultan2010@gmail.com>
2017-09-13 22:25:28 +0200

    Fixed notification.

--- src/app/components/song-info/song-info.component.ts
+++ src/app/components/song-info/song-info.component.ts
@@ -3,6 +3,7 @@ import {Artist} from '../../Models/artist';
 import {Song} from '../../Models/song';
 import {SongService} from '../../services/song.service';
 import {PlayerService} from "../../services/player.service";
+import {AppService} from "../../services/app.service";
 
 @Component({
   selector: 'app-song-info',
@@ -13,7 +14,9 @@ export class SongInfoComponent {
 
   @Input() song:Song;
 
-  constructor(private songService:SongService, private playerService:PlayerService) {
+  constructor(private songService:SongService,
+              private playerService:PlayerService,
+              public  appService:AppService) {
   }
 
 }
