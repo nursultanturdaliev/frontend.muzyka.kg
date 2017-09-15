24d1cb04d78dc6f4778b44739d62437b823c26f1
Adilet Egemberdiev <Adilet@aio.kg>
2017-09-15 12:13:59 +0600

    set topSongs to player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component, ElementRef, ViewChild, Renderer} from '@angular/core';
 import {PlayerService} from './services/player.service';
-import {SongService} from "./services/song.service";
-import {AuthService} from "./services/auth.service";
-import { FacebookService, InitParams } from 'ngx-facebook';
+import {SongService} from './services/song.service';
+import {AuthService} from './services/auth.service';
+import {FacebookService, InitParams} from 'ngx-facebook';
 import {Router} from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -14,12 +14,12 @@ import {Router} from '@angular/router';
 export class AppComponent {
   @ViewChild('toggleBtn') toggleBtn: ElementRef;
 
-  constructor(private playerService:PlayerService,
-              public authService:AuthService,
-              private fb:FacebookService,
-              private router:Router,
+  constructor(public playerService: PlayerService,
+              public authService: AuthService,
+              private fb: FacebookService,
+              private router: Router,
               private renderer: Renderer) {
-    let initParams:InitParams = {
+    const initParams: InitParams = {
       appId: '1974598029436106',
       cookie: true,
       xfbml: true,
@@ -30,13 +30,13 @@ export class AppComponent {
 
   }
 
-  public menuItemClick(){
-    this.renderer.invokeElementMethod(this.toggleBtn.nativeElement,'click');
-   console.log(this.toggleBtn);
+  public menuItemClick() {
+    this.renderer.invokeElementMethod(this.toggleBtn.nativeElement, 'click');
+    console.log(this.toggleBtn);
   }
 
   public logout() {
     this.authService.logout();
-    this.router.navigate(['/'])
+    this.router.navigate(['/']);
   }
 }
