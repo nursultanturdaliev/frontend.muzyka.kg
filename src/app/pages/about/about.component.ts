2abc72f801dbb57a7ce3b7b45282133d2b5023f5
Nursultan Turdaliev <nursultan2010@gmail.com>
2017-09-17 12:37:48 +0200

    Портал тууралуу баракчасы жаңы дизайнга өзгөртүлдү

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,4 @@
-/**
- * Created by nursultan on 9/11/17.
- */
 import {Component} from '@angular/core';
-import {Team} from "../../Models/Team";
-import {TeamMember} from '../../Models/TeamMember';
 
 @Component({
   selector: 'app-about',
@@ -11,15 +6,4 @@ import {TeamMember} from '../../Models/TeamMember';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  public team:Team;
-
-  constructor() {
-    this.team = new Team();
-    this.team.members.push(new TeamMember("Адилет", "Эгембердиев", "Программист",  "https://www.facebook.com/AdiletEgemberdievv", "","adilet.egemberdiev.jpg"));
-    this.team.members.push(new TeamMember("Айкут", "Конушбаев", "Универсал",  "https://www.facebook.com/Aykut.Konushbai", "","aykut.konushbai.jpg"));
-    this.team.members.push(new TeamMember("Бектур", "Өмүров", "Дизайнер",  "https://www.facebook.com/bekturomurov", "","bekturomurov.jpg"));
-    this.team.members.push(new TeamMember("Нурсултан", "Турдалиев", "Программист",  "https://www.facebook.com/turdaliev.nursultan", "https://www.linkedin.com/in/nurolopher/","nursultan.turdaliev.jpg"));
-    this.team.members.push(new TeamMember("Шерказы", "Кокумбаев", "Программист",  "https://www.facebook.com/sheki92m", "","sherkazy.kokumbaev.jpg"));
-    this.team.members.push(new TeamMember("Аким", "Болушбек уулу", "Программист",  "https://www.facebook.com/akim.bolushbekuulu", "","akim.bolushbekuulu.jpg"));
-  }
 }
