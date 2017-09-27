7c2fa4d66585cdeb79aba7ee5c56f8fab63bfa5e
Akimbolushbekclear <akim.bb@mail.ru>
2017-09-27 13:28:57 +0600

    Safari, iOS bug fixed

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -239,9 +239,14 @@ export class PlayerComponent implements OnInit {
   }
 
   public isIOS(){
+
+    var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+    if ((is_chrome)&&(is_safari)) {is_safari=false;}
+
     return parseFloat(
       ('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
         .replace('undefined', '3_2').replace('_', '.').replace('_', '')
-    ) || false;
+    ) || false || is_safari;
   }
 }
