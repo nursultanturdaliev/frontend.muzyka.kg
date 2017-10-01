c74745ac32bf4484a249e1b04908d8c86d8eaf07
Nursultan Turdaliev <nursultan2010@gmail.com>
2017-10-01 17:23:41 +0200

    Fixed bug!

--- src/app/pages/playlists/playlists.component.ts
+++ src/app/pages/playlists/playlists.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit,Inject} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PlaylistService} from "../../services/playlist.service";
 import {Playlist} from "../../Models/playlist";
-import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-playlists',
@@ -10,36 +9,15 @@ import {DOCUMENT} from '@angular/common';
 export class PlaylistsComponent implements OnInit {
 
   public playlists:Playlist[];
-  public page:number;
-  public noMorePlaylists:boolean;
-  public playlistsScrollContainer;
 
-  constructor(private playlistService:PlaylistService, @Inject(DOCUMENT) private document:any) {
-    this.page = 1;
+  constructor(private playlistService:PlaylistService) {
     this.playlists = [];
-    this.noMorePlaylists = false;
-    this.playlistsScrollContainer = this.document.querySelector('#bjax-target');
   }
 
   ngOnInit():void {
-    this.updatePlaylists();
-  }
-
-  onScrollDown():void{
-    this.page +=1;
-    this.updatePlaylists();
-  }
-
-  private updatePlaylists():void {
-    if (this.noMorePlaylists) {
-      return;
-    }
-    this.playlistService.getPlaylists(this.page)
+    this.playlistService.getPlaylists(1)
       .then(playlists => {
-        if (playlists.length === 0) {
-          this.noMorePlaylists = true;
-        }
-        Array.prototype.push.apply(this.playlists, playlists);
-      })
+        this.playlists = playlists;
+      });
   }
 }
